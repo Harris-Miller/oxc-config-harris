c93a4a6c0c0a966a3f428d61356664476e41d3a0
harris-miller <harrismillerconsulting@gmail.com>
2026-05-25 22:29:56 -0600

    add oxfmtrc

--- core/format.js
+++ core/format.js
@@ -1,30 +1,8 @@
+// oxfmt doesn't yet support "extends", but putting this here for when that's added
 const formatConfig = {
   arrowParens: "avoid",
   printWidth: 120,
   singleQuote: true,
-  semi: true,
-  trailingComma: "all",
-  tabWidth: 2,
-  useTabs: false,
-  bracketSpacing: true,
-  bracketSameLine: false,
-  jsxSingleQuote: false,
-  quoteProps: "as-needed",
-  endOfLine: "lf",
-  insertFinalNewline: true,
-  sortPackageJson: false,
-  sortImports: {
-    newlinesBetween: false,
-    groups: [
-      ["value-builtin", "value-external"],
-      ["value-internal", "value-parent", "value-sibling", "value-index"],
-      { newlinesBetween: true },
-      "type-import",
-      "unknown",
-    ],
-    order: "asc",
-    ignoreCase: true,
-  },
 };
 
 export default formatConfig;
